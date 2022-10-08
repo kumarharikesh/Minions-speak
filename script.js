31e5fb4744cc28cdb5e0684154f99952b0dcf6fe
Harikesh Patel <43793294+kumarharikesh@users.noreply.github.com>
2022-10-08 17:37:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const textInput = document.querySelector("#txt-input");
+const output = document.querySelector("#output");
+const form = document.querySelector("#form");
+
+let URL = "https://api.funtranslations.com/translate/minion.json";
+
+const getURL = (text) => `${URL}?text=${text}`;
+
+function errorHandler(error){
+  alert("Some error occured in the server.\nPlease try again after some time !\nSorry for the inconvenience. 🙂")
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const inputText = textInput.value;
+
+  fetch(getURL(inputText))
+    .then((response) => response.json())
+    .then((json) => (output.innerText = json.contents.translated))
+    .catch(errorHandler);
+});
